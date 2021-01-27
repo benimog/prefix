f0d0fb9f69fb701179c2c5c505ae542580aa78af
Martin Andersson <maran453@student.liu.se>
2021-01-27 23:42:28 +0100

    large numbers work, bad grammar

--- app.js
+++ app.js
@@ -148,6 +148,104 @@ $(document).ready(function () {
     }
  */
 
+    /* var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety']; */
+
+    /* function inWords (num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
+    return str;
+}
+
+document.getElementById('number').onkeyup = function () {
+    document.getElementById('words').innerHTML = inWords(document.getElementById('number').value);
+};
+ */
+
+    const th_val = ["", "tusen", "miljoner", "miljarder", "biljoner", "biljarder", "triljoner", "triljarder", "kvadriljoner"];
+
+    const dg_val = [
+      "noll",
+      "ett",
+      "två",
+      "tre",
+      "fyra",
+      "fem",
+      "sex",
+      "sju",
+      "åtta",
+      "nio",
+    ];
+    const tn_val = [
+      "tio",
+      "elva",
+      "tolv",
+      "tretton",
+      "fjorton",
+      "femton",
+      "sexton",
+      "sjutton",
+      "arton",
+      "nitton",
+    ];
+    const tw_val = [
+      "tjugo",
+      "trettio",
+      "fyrtio",
+      "femtio",
+      "sextio",
+      "sjuttio",
+      "åttio",
+      "nittio",
+    ];
+    function toWordsconver(s) {
+      // s = s.toString();
+      // s = s.replace(/[\, ]/g, "");
+      // if (s != parseFloat(s)) return "not a number ";
+      let x_val = s.indexOf(".");
+      if (x_val == -1) x_val = s.length;
+      if (x_val > 25) return "för stort :(";
+      const n_val = s.split("");
+      let str_val = "";
+      let sk_val = 0;
+      for (let i = 0; i < x_val; i++) {
+        if ((x_val - i) % 3 == 2) {
+          if (n_val[i] == "1") {
+            str_val += tn_val[Number(n_val[i + 1])] + " ";
+            i++;
+            sk_val = 1;
+          } else if (n_val[i] != 0) {
+            str_val += tw_val[n_val[i] - 2] + " ";
+            sk_val = 1;
+          }
+        } else if (n_val[i] != 0) {
+          str_val += dg_val[n_val[i]] + " ";
+          if ((x_val - i) % 3 == 0) str_val += "hundra ";
+          sk_val = 1;
+        }
+        if ((x_val - i) % 3 == 1) {
+          if (sk_val) str_val += th_val[(x_val - i - 1) / 3] + " ";
+          sk_val = 0;
+        }
+      }
+      if (x_val != s.length) {
+        const y_val = s.length;
+        str_val += "komma ";
+        for (let i = x_val + 1; i < y_val; i++)
+          str_val += dg_val[n_val[i]] + " ";
+      }
+      str_val.replace('ett', 'en');
+      return str_val.replace(/\s+/g, " ");
+    }
+
+    text = toWordsconver(number);
+
     // use modulo
 
     // number = "1234"
@@ -166,7 +264,7 @@ $(document).ready(function () {
 
     // text += prefix(number);
 
-    if (parseInt(number) < 20) {
+    /*    if (parseInt(number) < 20) {
       text = digitAsText(number);
     } else {
       let stack = [];
@@ -181,12 +279,12 @@ $(document).ready(function () {
       stack.push(prefix(number.substring(1)));
       stack.push(prefix(number.substring(0)));  
       
-
+      console.log(number);
       
       while (stack.length !== 0) {
         text += stack.pop();
       }
-    }
+    } */
 
     /* if (parseInt(number) < 20) {
       text = digitAsText(number);
