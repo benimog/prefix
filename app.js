90bb350f8373565fda8514724515e9691607746e
Martin Andersson <maran453@student.liu.se>
2021-01-28 18:32:56 +0100

    regex loop

--- app.js
+++ app.js
@@ -105,14 +105,54 @@ $(document).ready(function () {
       for (let i = x_val + 1; i < y_val; i++) str_val += dg_val[n_val[i]] + " ";
     }
 
-    str_val = str_val.replace(/tio\ (?=\w)/g, "tio");
-    str_val = str_val.replace(/ett miljoner/g, "en miljon");
-    str_val = str_val.replace(/ett miljarder/g, "en miljard");
+    // str_val = str_val.replace(/tio\ (?=\w)/g, "tio");
+    str_val = str_val.replace(/tjugo\ (?=\w)/g, "tjugo");
+    str_val = str_val.replace(/trettio\ (?=\w)/g, "trettio");
+    str_val = str_val.replace(/fyrtio\ (?=\w)/g, "fyrtio");
+    str_val = str_val.replace(/femtio\ (?=\w)/g, "femtio");
+    str_val = str_val.replace(/sextio\ (?=\w)/g, "sextio");
+    str_val = str_val.replace(/sjuttio\ (?=\w)/g, "sjuttio");
+    str_val = str_val.replace(/åttio\ (?=\w)/g, "åttio");
+    str_val = str_val.replace(/nittio\ (?=\w)/g, "nittio");
+
+    // str_val = str_val.replace(/hundra\ (?=\w)/g, "hundra");
+    /*     str_val = str_val.replace(/ett miljoner/g, "en miljon");
+    str_val = str_val.replace(/\wen miljon/g, "en miljoner"); */
+    /*     str_val = str_val.replace(/ett miljarder/g, "en miljard");
+    str_val = str_val.replace(/\wen miljard/g, "en miljarder");
     str_val = str_val.replace(/ett biljoner/g, "en biljon");
     str_val = str_val.replace(/ett biljarder/g, "en biljard");
     str_val = str_val.replace(/ett triljoner/g, "en triljard");
     str_val = str_val.replace(/ett triljarder/g, "en triljard");
-    str_val = str_val.replace(/ett kvadriljoner/g, "en kvadriljon");
+    str_val = str_val.replace(/ett kvadriljoner/g, "en kvadriljon"); */
+
+    for (i = 2; i < th_val.length; ++i) {
+      let regex = new RegExp("ett " + th_val[i], "g");
+      str_val = str_val.replace(
+        regex,
+        `oen ${th_val[i].substring(0, th_val[i].length - 2)}`
+      );
+
+      /*       let regex2 = new RegExp("\Wen " + th_val[i],"g");
+      str_val = str_val.replace(regex2, `en ${th_val[i]}`); */
+
+      /*       regex2 = new RegExp("\wen " + th_val[i].substring(0, th_val[i].length-2),"g");
+      str_val = str_val.replace(regex2, `en ${th_val[i]}`) */
+    }
+    str_val = str_val.replace(/\wen miljon/g, "en miljoner");
+    str_val = str_val.replace(/\wen miljard/g, "en miljarder"); 
+    str_val = str_val.replace(/\wen biljon/g, "en biljoner"); 
+    str_val = str_val.replace(/\wen biljard/g, "en biljarder"); 
+    str_val = str_val.replace(/\wen triljon/g, "en triljoner"); 
+    str_val = str_val.replace(/\wen triljard/g, "en triljarder");
+    str_val = str_val.replace(/\wen kvadriljon/g, "en kvadriljoner");  
+
+
+    /*     for (i = 0; i < tw_val.length; ++i) {
+      let regex = new RegExp(tw_val[i] + "\ (?=\w)","g");
+      str_val = str_val.replace(regex, tw_val[i]);
+      console.log(`i: ${i}; regex: "${regex}"; tw_val[i]: "${tw_val[i]}"; str_val: "${str_val}"`);
+    } */
 
     return str_val.replace(/\s+/g, " ");
   }
